fd8c634f213bdc701d8f064926d80288b4f9ca5a
enhuiz <niuzhe.nz@outlook.com>
2023-01-10 14:49:22 +0800

    Train more & Tune

--- src/config.py
+++ src/config.py
@@ -23,11 +23,12 @@ class Config(ConfigBase):
     eval_batch_size: int = 512
     warmup_min_lr: float = 1e-6
     warmup_max_lr: float = 2e-4
-    warmup_num_steps: int = 5_00
-    max_iter: int = 5_000
+    dis_warmup_max_lr: float = 4e-4
+    warmup_num_steps: int = 1_000
+    max_iter: int = 10_000
     gradient_clipping: float = 100
-    eval_every: int = 1_000
-    save_ckpt_every: int = 5_000
+    eval_every: int = 2_000
+    save_ckpt_every: int = 10_000
 
     constant_lr: float | None = None
 
@@ -43,7 +44,7 @@ class Config(ConfigBase):
         return round(math.exp(self.vae_target_kl))
 
     @property
-    def warmup_decay_scheduler(self):
+    def gen_warmup_decay_scheduler(self):
         return {
             "type": "WarmupDecayLR",
             "params": {
@@ -56,31 +57,28 @@ class Config(ConfigBase):
         }
 
     @property
-    def constant_scheduler(self):
+    def dis_warmup_decay_scheduler(self):
         return {
-            "type": "WarmupLR",
+            "type": "WarmupDecayLR",
             "params": {
-                # Keep the same
-                "warmup_min_lr": self.constant_lr,
-                "warmup_max_lr": self.constant_lr,
-                "warmup_num_steps": 1,
+                "warmup_min_lr": self.warmup_min_lr,
+                "warmup_max_lr": self.dis_warmup_max_lr,
+                "warmup_num_steps": self.warmup_num_steps,
+                "total_num_steps": self.max_iter,
+                "warmup_type": "linear",
             },
         }
-
-    @property
-    def scheduler(self):
-        if self.constant_lr is not None:
-            return self.constant_scheduler
-        else:
-            return self.warmup_decay_scheduler
 
     @property
     def gen_ds_cfg(self):
         return {
             "train_micro_batch_size_per_gpu": self.batch_size,
             "gradient_accumulation_steps": 1,
-            "optimizer": {"type": "Adam"},
-            "scheduler": self.scheduler,
+            "optimizer": {
+                "type": "Adam",
+                "betas": (0.0, 0.999),
+            },
+            "scheduler": self.gen_warmup_decay_scheduler,
             "gradient_clipping": self.gradient_clipping,
         }
 
@@ -89,8 +87,11 @@ class Config(ConfigBase):
         return {
             "train_micro_batch_size_per_gpu": self.batch_size,
             "gradient_accumulation_steps": 1,
-            "optimizer": {"type": "Adam"},
-            "scheduler": self.scheduler,
+            "optimizer": {
+                "type": "Adam",
+                "betas": (0.0, 0.999),
+            },
+            "scheduler": self.dis_warmup_decay_scheduler,
             "gradient_clipping": self.gradient_clipping,
         }
 
