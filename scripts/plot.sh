c0f46883f7164ef49a2de36974fba0ea41cb5d9a
enhuiz <niuzhe.nz@outlook.com>
2023-01-10 17:25:21 +0800

    Update line style

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 mkdir -p figures/$1
 python -m src.plot fid --output figures/$1/fid.png ${@:2}
 python -m src.plot generator.loss discriminator.loss --output figures/$1/loss.png ${@:2}
--- src/plot.py
+++ src/plot.py
@@ -51,6 +51,10 @@ def plot(paths, args):
             if gdf[y].isna().all():
                 continue
 
+            names = gdf["name"].unique()
+            assert len(names) == 1, "One group should have at most 1 name."
+            linestyle = dict(train_200="--", test="-").get(names[0], "-.")
+
             gdf.plot(
                 x="global_step",
                 y=y,
@@ -58,6 +62,7 @@ def plot(paths, args):
                 ax=plt.gca(),
                 marker="x" if len(gdf) < 100 else None,
                 alpha=0.7,
+                linestyle=linestyle,
             )
 
     plt.gca().legend(
